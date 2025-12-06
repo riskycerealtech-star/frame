9f4942dd84a21e3f12b58897373186e78a7d324a
Deodate <ajadeo04@gmail.com>
2025-12-06 22:45:26 +0200

    chore(docs): rename title from 'Frame API' to 'Flame APIs'

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from config import settings
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
-    title="Frame API",
+    title="Flame APIs",
     version="1.0.0",
     description="",
     contact={
